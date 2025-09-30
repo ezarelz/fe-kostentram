986a6b1d01ae0305fea25d9f3f8f595da98d4914
ezarelz <manggala.proflight@gmail.com>
2025-10-01 01:13:26 +0700

    first commit

--- app/robot.ts
+++ app/robot.ts
@@ -1,11 +1,12 @@
 // app/robots.ts
-import { MetadataRoute } from 'next';
+import type { MetadataRoute } from 'next';
+
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: {
       userAgent: '*',
       allow: '/',
     },
-    sitemap: 'https://www.kostentram.my.id/sitemap.xml',
+    sitemap: 'https://kostentram.my.id/sitemap.xml',
   };
 }
